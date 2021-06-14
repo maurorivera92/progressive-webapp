5788688ffdd39a4875fd843e764ef06aef7cd152
Mauro Rivera <mauro.rivera@glr.pe>
2021-06-14 01:23:20 -0500

    Added first views

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import MainTemplate from "../layouts/main";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <MainTemplate>
+            <Component {...pageProps} />
+        </MainTemplate>
+    );
 }
-export default MyApp
+export default MyApp;
